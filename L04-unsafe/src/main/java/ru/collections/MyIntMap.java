db2a05fb8eb65ef176a5d78eb8af03005f51195f
iakonyakina <iakonyakina@domclick.ru>
2021-08-01 00:19:26 +0300

    my collection with unsafe

--- L04-unsafe/src/main/java/ru/collections/MyIntMap.java
+++ L04-unsafe/src/main/java/ru/collections/MyIntMap.java
@@ -1,4 +1,4 @@
-package ru.otus;
+package ru.collections;
 
 public class MyIntMap {
 
--- L04-unsafe/src/test/java/ru/collections/MyIntArrayTest.java
+++ L04-unsafe/src/test/java/ru/collections/MyIntArrayTest.java
@@ -1,4 +1,4 @@
-package ru.otus;
+package ru.collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
