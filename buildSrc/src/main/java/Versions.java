95e3a139d395905b0decb4c6d49820160772e39f
iakonyakina <iakonyakina@domclick.ru>
2021-11-01 02:08:32 +0300

    Up java version from 15 to 17. Up gradle from gradle-7.1-all to gradle-7.2-bin

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,6 +1,7 @@
 public interface Versions {
-    String guava = "30.1.1-jre";
+    String guava = "28.2-jre";
     String jmh = "1.32";
+    String jol = "0.16";
     String asm = "9.1";
     String glassfishJson = "1.1.4";
     String protobuf = "3.12.1";
@@ -20,4 +21,6 @@ public interface Versions {
 
     String jetty = "11.0.6";
     String freemarker = "2.3.31";
+
+    String reflections = "0.9.12";
 }
