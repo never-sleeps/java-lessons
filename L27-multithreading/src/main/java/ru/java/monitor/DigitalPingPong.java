c0e5b9f727eb536da21ae1fa8a8c79db979eaf94
iakonyakina <iakonyakina@domclick.ru>
2021-12-28 11:27:16 +0300

    Digital PingPong

--- L27-multithreading/src/main/java/ru/java/monitor/DigitalPingPong.java
+++ L27-multithreading/src/main/java/ru/java/monitor/DigitalPingPong.java
@@ -18,22 +18,17 @@ public class DigitalPingPong {
     private static final String THREAD_NAME_1 = "t1";
     private static final String THREAD_NAME_2 = "t2";
 
-    private final Iterator<Integer> t1NumberSequence;
-    private final Iterator<Integer> t2NumberSequence;
-
     private String lastThreadName;
-
-    public DigitalPingPong() {
-        this.t1NumberSequence = generateNumberSequence();
-        this.t2NumberSequence = generateNumberSequence();
-    }
 
     public static void main(String[] args) {
         DigitalPingPong pingPong = new DigitalPingPong();
+        Iterator<Integer> t1NumberSequence = generateNumberSequence();
+        Iterator<Integer> t2NumberSequence = generateNumberSequence();
 
         pingPong.lastThreadName = THREAD_NAME_2; // таким образом фиксируем, чтобы первым начал работу t1
-        Thread t1 = new Thread(pingPong::action);
-        Thread t2 = new Thread(pingPong::action);
+
+        Thread t1 = new Thread(() -> pingPong.action(t1NumberSequence));
+        Thread t2 = new Thread(() -> pingPong.action(t2NumberSequence));
 
         t1.setName(THREAD_NAME_1);
         t2.setName(THREAD_NAME_2);
@@ -41,7 +36,7 @@ public class DigitalPingPong {
         t2.start();
     }
 
-    private synchronized void action() {
+    private synchronized void action(Iterator<Integer> numberSequence) {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 while (lastThreadName.equals(Thread.currentThread().getName())) {
@@ -49,12 +44,7 @@ public class DigitalPingPong {
                 }
 
                 lastThreadName = Thread.currentThread().getName();
-
-                if (lastThreadName.equals(THREAD_NAME_1)) {
-                    print(t1NumberSequence);
-                } else {
-                    print(t2NumberSequence);
-                }
+                print(numberSequence);
 
                 sleep((lastThreadName.equals(THREAD_NAME_1)) ? 500 : 400);
                 notifyAll();
@@ -64,6 +54,10 @@ public class DigitalPingPong {
         }
     }
 
+    /**
+     * Если в [numberSequence] есть элемент для печати, печатаем его, иначе - прерываем поток
+     * @param numberSequence - последовательность элементов для печати
+     */
     private void print(Iterator<Integer> numberSequence) {
         if (numberSequence.hasNext()) {
             var valueForPrint = numberSequence.next();
@@ -71,7 +65,7 @@ public class DigitalPingPong {
         } else Thread.currentThread().interrupt();
     }
 
-    private Iterator<Integer> generateNumberSequence() {
+    private static Iterator<Integer> generateNumberSequence() {
         List<Integer> list = new ArrayList<>(20);
         for (int i = 1; i <= 10; i++) {
             list.add(i);
